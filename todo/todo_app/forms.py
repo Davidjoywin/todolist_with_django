cc0c2abd3b03a842aae69d6241a736f3cdbc369b
david joy <davidjoy@2k18@gmail.com>
2022-07-13 19:17:11 +0100

    ...

--- todo/todo_app/forms.py
+++ todo/todo_app/forms.py
@@ -7,19 +7,19 @@ from todo_app.models import Note
 class CreateUserForm(UserCreationForm):
     password1= forms.CharField(
         widget = forms.PasswordInput(attrs={
-            "class": "block w-8 px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
+            "class": "block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
             }))
 
     password2= forms.CharField(
         widget = forms.PasswordInput(
-            attrs={"class": "block w-8 px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 focus:outline-none focus:ring"
+            attrs={"class": "block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
     }))
 
     class Meta:
         model = User
         fields = ["username", "email", "password1", "password2"]
         widgets = {
-            "username": forms.TextInput(attrs={"class": "block w-8 px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 focus:outline-none focus:ring"}),
-            "email": forms.EmailInput(attrs={"class": "block w-8 px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 focus:outline-none focus:ring"}),
+            "username": forms.TextInput(attrs={"class": "block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"}),
+            "email": forms.EmailInput(attrs={"class": "block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"}),
             }
         
